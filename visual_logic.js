ebab8e0f9d8f6210bd73a0ae6aea76652cb0ff26
Nguyễn Tiến Đạt <datlacve2@gmail.com>
2025-07-05 12:33:41 +0700

    Update visual_logic.js

--- visual_logic.js
+++ visual_logic.js
@@ -13722,7 +13722,7 @@ Thêm Ảnh
     
         console.log('🚀 Uploading layout to server...', { filename });
     
-        fetch('https://inkme-3d-server-production.up.railway.app/api/products/upload-file'), {
+        fetch('https://inkme-3d-server-production.up.railway.app/api/products/upload-file', {
           method: 'POST',
          headers: {
         'Authorization': authorization,
@@ -13769,7 +13769,7 @@ Thêm Ảnh
           ]
         };
     
-          return fetch(https://inkme-3d-server-production.up.railway.app/api/cart/add'), {
+          return fetch('https://inkme-3d-server-production.up.railway.app/api/cart/add', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
